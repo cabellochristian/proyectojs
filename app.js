7adf99bc8cd5129e1a22b163686834349ecf65b3
cabellochristian <cabello.christian.e@gmail.com>
2023-09-19 23:29:59 -0300

    preentrega2

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 class Empleado {
-    constructor(nombre, edad, departamento) {
+    constructor(nombre, edad, departamento, proyectos, habilidades) {
         this.nombre = nombre;
         this.edad = edad;
         this.departamento = departamento;
+        this.proyectos = proyectos;
+        this.habilidades = habilidades;
     }
 }
 
 let empleados = [
-    new Empleado("Juan", 30, "Ventas"),
-    new Empleado("María", 28, "Recursos Humanos"),
-    new Empleado("Carlos", 35, "Tecnología"),
-    new Empleado("Ana", 40, "Finanzas"),
+    new Empleado("Juan", 30, "Ventas", ["Proyecto A", "Proyecto B"], ["Ventas", "Comunicación"]),
+    new Empleado("Maria", 28, "Recursos Humanos", ["Proyecto C"], ["Gestión de Personal"]),
+    new Empleado("Carlos", 35, "Tecnología", ["Proyecto D", "Proyecto E"], ["Desarrollo Web", "Programación"]),
+    new Empleado("Ana", 40, "Finanzas", ["Proyecto F"], ["Contabilidad", "Finanzas"]),
 ];
 
 let acceso = prompt("Si quieres acceder a la lista de empleados, ingresa la contraseña:");
@@ -20,22 +22,87 @@ while (acceso !== "1234") {
     acceso = prompt("Si quieres acceder a la lista de empleados, ingresa la contraseña:");
 }
 
-let seleccion;
+let opcion;
 
 while (acceso === "1234") {
-    seleccion = parseInt(prompt("Selecciona al empleado al que quieres acceder:\n1. Juan\n2. María\n3. Carlos\n4. Ana\n5. Salir"));
+    opcion = parseInt(prompt("Selecciona una opción:\n1. Mostrar detalles de un empleado por nombre\n2. Filtrar empleados por departamento\n3. Obtener lista de proyectos\n4. Calcular edad promedio\n5. Salir"));
 
-    if (seleccion >= 1 && seleccion <= 4) {
-        mostrarDetalleEmpleado(empleados[seleccion - 1]);
-    } else if (seleccion === 5) {
-        acceso = prompt("Si quieres acceder a la lista de empleados, ingresa la contraseña:");
-    } else {
-        alert('Selecciona un número válido o 5 para salir.');
+    switch (opcion) {
+        case 1:
+            const nombreEmpleado = prompt("Ingresa el nombre del empleado:");
+            buscarEmpleadoPorNombre(nombreEmpleado);
+            break;
+        case 2:
+            const departamentoEmpleado = prompt("Ingresa el departamento:");
+            filtrarEmpleadosPorDepartamento(departamentoEmpleado);
+            break;
+        case 3:
+            obtenerListaDeProyectos();
+            break;
+        case 4:
+            calcularEdadPromedio();
+            break;
+        case 6:
+            acceso = prompt("Si quieres acceder a la lista de empleados, ingresa la contraseña:");
+            break;
+        default:
+            alert("Selecciona una opción válida o 5 para salir.");
     }
 }
 
-function mostrarDetalleEmpleado(empleado) {
-    for (let key in empleado) {
-        alert(key + ": " + empleado[key]);
+function buscarEmpleadoPorNombre(nombre) {
+    const empleadoEncontrado = empleados.find(empleado => empleado.nombre === nombre);
+    if (empleadoEncontrado) {
+        mostrarDetalleEmpleado(empleadoEncontrado);
+    } else {
+        alert(`No se encontró ningún empleado con el nombre ${nombre}`);
     }
+}
+
+function filtrarEmpleadosPorDepartamento(departamento) {
+    const empleadosFiltrados = empleados.filter(empleado => empleado.departamento === departamento);
+    if (empleadosFiltrados.length > 0) {
+        empleadosFiltrados.forEach(empleado => mostrarDetalleEmpleado(empleado));
+    } else {
+        alert(`No se encontraron empleados en el departamento ${departamento}`);
+    }
+}
+
+function obtenerListaDeProyectos() {
+    const proyectos = [];
+    empleados.forEach(empleado => {
+        proyectos.push(...empleado.proyectos);
+    });
+    const proyectosUnicos = [...new Set(proyectos)];
+    alert(`Lista de proyectos:\n${proyectosUnicos.join('\n')}`);
+}
+
+function obtenerListaDeEmpleados() {
+    const proyectos = [];
+    empleados.forEach(empleado => {
+        proyectos.push(...empleado.proyectos);
+    });
+    const proyectosUnicos = [...new Set(proyectos)];
+    alert(`Lista de proyectos:\n${proyectosUnicos.join('\n')}`);
+}
+
+function calcularEdadPromedio() {
+    const edades = empleados.map(empleado => empleado.edad);
+    const sumaEdades = edades.reduce((total, edad) => total + edad, 0);
+    const promedioEdad = sumaEdades / empleados.length;
+    alert(`Edad promedio de los empleados: ${promedioEdad.toFixed(2)}`);
+}
+
+function mostrarDetalleEmpleado(empleado) {
+    alert(`Nombre: ${empleado.nombre}\nEdad: ${empleado.edad}\nDepartamento: ${empleado.departamento}`);
+    
+    alert("Proyectos:");
+    empleado.proyectos.forEach((proyecto, index) => {
+        alert(`${index + 1}. ${proyecto}`);
+    });
+
+    alert("Habilidades:");
+    empleado.habilidades.forEach((habilidad, index) => {
+        alert(`${index + 1}. ${habilidad}`);
+    });
 }
